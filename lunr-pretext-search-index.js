c2d2891f922077e83ce2739f3ff069a7d17a52b2
NathanBrownlowe <156858262+NathanBrownlowe@users.noreply.github.com>
2025-12-11 07:38:10 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -55,112 +55,418 @@ var ptx_lunr_docs = [
   "body": " Vector Spaces  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-dot-products",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-dot-products.html",
   "type": "Section",
   "number": "2.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Dot products",
+  "body": " Dot products  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-angle-between-vectors",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-angle-between-vectors.html",
+  "type": "Section",
+  "number": "2.2",
+  "title": "Angle between vectors",
+  "body": " Angle between vectors  Text of section.  "
+},
+{
+  "id": "sec-projections",
+  "level": "1",
+  "url": "sec-projections.html",
+  "type": "Section",
+  "number": "2.3",
+  "title": "Projections",
+  "body": " Projections  Text of section.  "
+},
+{
+  "id": "sec-cross-products",
+  "level": "1",
+  "url": "sec-cross-products.html",
   "type": "Section",
   "number": "3.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Cross products",
+  "body": " Cross products  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-geo-apps-of-cross-products",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-geo-apps-of-cross-products.html",
+  "type": "Section",
+  "number": "3.2",
+  "title": "Geometric applications of cross products",
+  "body": " Geometric applications of cross products  Text of section.  "
+},
+{
+  "id": "sec-lines-in-R2",
+  "level": "1",
+  "url": "sec-lines-in-R2.html",
   "type": "Section",
   "number": "4.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Lines in <span class=\"process-math\">\\(\\mathbb{R}^2\\)<\/span>",
+  "body": " Lines in  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-lines-in-R3-and-Rn",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-lines-in-R3-and-Rn.html",
+  "type": "Section",
+  "number": "4.2",
+  "title": "Lines in <span class=\"process-math\">\\(\\mathbb{R}^3\\)<\/span> and <span class=\"process-math\">\\(\\mathbb{R}^n\\)<\/span>",
+  "body": " Lines in and  Text of section.  "
+},
+{
+  "id": "sec-planes",
+  "level": "1",
+  "url": "sec-planes.html",
+  "type": "Section",
+  "number": "4.3",
+  "title": "Planes",
+  "body": " Planes  Text of section.  "
+},
+{
+  "id": "sec-shortest-distance",
+  "level": "1",
+  "url": "sec-shortest-distance.html",
+  "type": "Section",
+  "number": "4.4",
+  "title": "Shortest distance from a point to a plane",
+  "body": " Shortest distance from a point to a plane  Text of section.  "
+},
+{
+  "id": "sec-systems",
+  "level": "1",
+  "url": "sec-systems.html",
   "type": "Section",
   "number": "5.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Systems of linear equations",
+  "body": " Systems of linear equations  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-guassian-elimination",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-guassian-elimination.html",
+  "type": "Section",
+  "number": "5.2",
+  "title": "Gaussian elimination",
+  "body": " Gaussian elimination  Text of section.  "
+},
+{
+  "id": "sec-number-of-solutions",
+  "level": "1",
+  "url": "sec-number-of-solutions.html",
+  "type": "Section",
+  "number": "5.3",
+  "title": "Number of solutions",
+  "body": " Number of solutions  Text of section.  "
+},
+{
+  "id": "sec-gauss-jordan-elimination",
+  "level": "1",
+  "url": "sec-gauss-jordan-elimination.html",
+  "type": "Section",
+  "number": "5.4",
+  "title": "Gauss-Jordan elimination",
+  "body": " Gauss-Jordan elimination  Text of section.  "
+},
+{
+  "id": "sec-matrices",
+  "level": "1",
+  "url": "sec-matrices.html",
   "type": "Section",
   "number": "6.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Matrices",
+  "body": " Matrices  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-matrix-add-and-scale",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-matrix-add-and-scale.html",
+  "type": "Section",
+  "number": "6.2",
+  "title": "Matrix addition and scalar multiplication",
+  "body": " Matrix addition and scalar multiplication  Text of section.  "
+},
+{
+  "id": "sec-matrix-mult",
+  "level": "1",
+  "url": "sec-matrix-mult.html",
+  "type": "Section",
+  "number": "6.3",
+  "title": "Matrix multiplication",
+  "body": " Matrix multiplication  Text of section.  "
+},
+{
+  "id": "sec-transpose",
+  "level": "1",
+  "url": "sec-transpose.html",
+  "type": "Section",
+  "number": "6.4",
+  "title": "Transpose",
+  "body": " Transpose  Text of section.  "
+},
+{
+  "id": "sec-properties-of-inverses",
+  "level": "1",
+  "url": "sec-properties-of-inverses.html",
   "type": "Section",
   "number": "7.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Properties of inverses",
+  "body": " Properties of inverses  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-calculating-2x2-inverses",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-calculating-2x2-inverses.html",
+  "type": "Section",
+  "number": "7.2",
+  "title": "Calculating <span class=\"process-math\">\\(2\\times 2\\)<\/span> inverses",
+  "body": " Calculating inverses  Text of section.  "
+},
+{
+  "id": "sec-calculating-nxn-inverses",
+  "level": "1",
+  "url": "sec-calculating-nxn-inverses.html",
+  "type": "Section",
+  "number": "7.3",
+  "title": "Calculating <span class=\"process-math\">\\(n\\times n\\)<\/span> inverses",
+  "body": " Calculating inverses  Text of section.  "
+},
+{
+  "id": "sec-elementary-matrices",
+  "level": "1",
+  "url": "sec-elementary-matrices.html",
+  "type": "Section",
+  "number": "7.4",
+  "title": "Elementary matrices",
+  "body": " Elementary matrices  Text of section.  "
+},
+{
+  "id": "sec-characterisations-of-invertibility",
+  "level": "1",
+  "url": "sec-characterisations-of-invertibility.html",
+  "type": "Section",
+  "number": "7.5",
+  "title": "Characterisations of invertibility",
+  "body": " Characterisations of invertibility  Text of section.  "
+},
+{
+  "id": "sec-calculating-nxn-determinants",
+  "level": "1",
+  "url": "sec-calculating-nxn-determinants.html",
   "type": "Section",
   "number": "8.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Calculating determinants",
+  "body": " Calculating determinants  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-props-of-determinants",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-props-of-determinants.html",
+  "type": "Section",
+  "number": "8.2",
+  "title": "Properties of determinants",
+  "body": " Properties of determinants  Text of section.  "
+},
+{
+  "id": "sec-span",
+  "level": "1",
+  "url": "sec-span.html",
   "type": "Section",
   "number": "9.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Span",
+  "body": " Span  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-linear-independence",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-linear-independence.html",
+  "type": "Section",
+  "number": "9.2",
+  "title": "Linear independence",
+  "body": " Linear independence  Text of section.  "
+},
+{
+  "id": "sec-subspaces",
+  "level": "1",
+  "url": "sec-subspaces.html",
   "type": "Section",
   "number": "10.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Subspaces",
+  "body": " Subspaces  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-row-and-column-spaces",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-row-and-column-spaces.html",
+  "type": "Section",
+  "number": "10.2",
+  "title": "Row and column spaces",
+  "body": " Row and column spaces  Text of section.  "
+},
+{
+  "id": "sec-null-spaces",
+  "level": "1",
+  "url": "sec-null-spaces.html",
+  "type": "Section",
+  "number": "10.3",
+  "title": "Null spaces",
+  "body": " Null spaces  Text of section.  "
+},
+{
+  "id": "sec-bases",
+  "level": "1",
+  "url": "sec-bases.html",
   "type": "Section",
   "number": "11.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Bases",
+  "body": " Bases  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-dimension",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-dimension.html",
+  "type": "Section",
+  "number": "11.2",
+  "title": "Dimension",
+  "body": " Dimension  Text of section.  "
+},
+{
+  "id": "sec-rank-nullity-theorem",
+  "level": "1",
+  "url": "sec-rank-nullity-theorem.html",
+  "type": "Section",
+  "number": "11.3",
+  "title": "The rank-nullity theorem",
+  "body": " The rank-nullity theorem  Text of section.  "
+},
+{
+  "id": "sec-evalues-evectors-espaces",
+  "level": "1",
+  "url": "sec-evalues-evectors-espaces.html",
   "type": "Section",
   "number": "12.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Eigenvalues, eigenvectors, and eigenspaces",
+  "body": " Eigenvalues, eigenvectors, and eigenspaces  Text of section.  "
 },
 {
-  "id": "sec-section-name",
+  "id": "sec-evalue-examples",
   "level": "1",
-  "url": "sec-section-name.html",
+  "url": "sec-evalue-examples.html",
+  "type": "Section",
+  "number": "12.2",
+  "title": "Examples",
+  "body": " Examples  Text of section.  "
+},
+{
+  "id": "sec-evalue-multiplicities",
+  "level": "1",
+  "url": "sec-evalue-multiplicities.html",
+  "type": "Section",
+  "number": "12.3",
+  "title": "Eigenvalue multiplicities",
+  "body": " Eigenvalue multiplicities  Text of section.  "
+},
+{
+  "id": "sec-characterisations-of-invertibility-take2",
+  "level": "1",
+  "url": "sec-characterisations-of-invertibility-take2.html",
+  "type": "Section",
+  "number": "12.4",
+  "title": "Characterisations of invertibility (take 2!)",
+  "body": " Characterisations of invertibility (take 2!)  Text of section.  "
+},
+{
+  "id": "sec-similarity",
+  "level": "1",
+  "url": "sec-similarity.html",
   "type": "Section",
   "number": "13.1",
-  "title": "Section Title",
-  "body": " Section Title  Text of section.  "
+  "title": "Similarity",
+  "body": " Similarity  Text of section.  "
+},
+{
+  "id": "sec-diagonalisation",
+  "level": "1",
+  "url": "sec-diagonalisation.html",
+  "type": "Section",
+  "number": "13.2",
+  "title": "Diagonalisation",
+  "body": " Diagonalisation  Text of section.  "
+},
+{
+  "id": "sec-examples-of-diagonalisation",
+  "level": "1",
+  "url": "sec-examples-of-diagonalisation.html",
+  "type": "Section",
+  "number": "13.3",
+  "title": "Examples of diagonalisation",
+  "body": " Examples of diagonalisation  Text of section.  "
+},
+{
+  "id": "sec-apps-of-diagonalisation",
+  "level": "1",
+  "url": "sec-apps-of-diagonalisation.html",
+  "type": "Section",
+  "number": "13.4",
+  "title": "Applications of diagonalisation",
+  "body": " Applications of diagonalisation  Text of section.  "
+},
+{
+  "id": "sec-prob-vectors-and-stochastic-matrices",
+  "level": "1",
+  "url": "sec-prob-vectors-and-stochastic-matrices.html",
+  "type": "Section",
+  "number": "14.1",
+  "title": "Probability vectors and stochastic matrices",
+  "body": " Probability vectors and stochastic matrices  Text of section.  "
+},
+{
+  "id": "sec-markov-chains",
+  "level": "1",
+  "url": "sec-markov-chains.html",
+  "type": "Section",
+  "number": "14.2",
+  "title": "Markov chains",
+  "body": " Markov chains  Text of section.  "
+},
+{
+  "id": "sec-steady-state-vectors",
+  "level": "1",
+  "url": "sec-steady-state-vectors.html",
+  "type": "Section",
+  "number": "14.3",
+  "title": "Steady state vectors",
+  "body": " Steady state vectors  Text of section.  "
+},
+{
+  "id": "sec-regular-markov-chains",
+  "level": "1",
+  "url": "sec-regular-markov-chains.html",
+  "type": "Section",
+  "number": "14.4",
+  "title": "Regular Markov chains",
+  "body": " Regular Markov chains  Text of section.  "
+},
+{
+  "id": "sec-pop-models-and-leslie-matrices",
+  "level": "1",
+  "url": "sec-pop-models-and-leslie-matrices.html",
+  "type": "Section",
+  "number": "15.1",
+  "title": "Population models and Leslie matrices",
+  "body": " Population models and Leslie matrices  Text of section.  "
+},
+{
+  "id": "sec-long-term-behaviour",
+  "level": "1",
+  "url": "sec-long-term-behaviour.html",
+  "type": "Section",
+  "number": "15.2",
+  "title": "Long term behaviour",
+  "body": " Long term behaviour  Text of section.  "
 },
 {
   "id": "backmatter-2",
